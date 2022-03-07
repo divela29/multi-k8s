b22b6ef6cf22b93b793a820b342f3a7dd7f8f51b
Daniel Vela <divela@minsait.com>
2022-03-07 16:12:34 -0600

    Correccion de archivos travis y deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t divela/multi-client-k8s:latest -t divela/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t divela/multi-server-k8s-pgfix:latest -t divela/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t divela/multi-worker-k8s:latest -t divela/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push cygnetops/multi-client-k8s:latest
-docker push cygnetops/multi-server-k8s-pgfix:latest
-docker push cygnetops/multi-worker-k8s:latest
+docker push divela/multi-client-k8s:latest
+docker push divela/multi-server-k8s-pgfix:latest
+docker push divela/multi-worker-k8s:latest
 
-docker push cygnetops/multi-client-k8s:$SHA
-docker push cygnetops/multi-server-k8s-pgfix:$SHA
-docker push cygnetops/multi-worker-k8s:$SHA
+docker push divela/multi-client-k8s:$SHA
+docker push divela/multi-server-k8s-pgfix:$SHA
+docker push divela/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=divela/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=divela/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=divela/multi-worker-k8s:$SHA
