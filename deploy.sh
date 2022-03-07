4de9a721a2ff983147b43981f14b62566754dde9
Daniel Vela <divela@minsait.com>
2022-03-07 16:06:18 -0600

    Correccion de archivos travis y deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client-k8s:latest -t stephengrider/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server-k8s:latest -t stephengrider/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker-k8s:latest -t stephengrider/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client-k8s:latest
-docker push stephengrider/multi-server-k8s:latest
-docker push stephengrider/multi-worker-k8s:latest
+docker push cygnetops/multi-client-k8s:latest
+docker push cygnetops/multi-server-k8s-pgfix:latest
+docker push cygnetops/multi-worker-k8s:latest
 
-docker push stephengrider/multi-client-k8s:$SHA
-docker push stephengrider/multi-server-k8s:$SHA
-docker push stephengrider/multi-worker-k8s:$SHA
+docker push cygnetops/multi-client-k8s:$SHA
+docker push cygnetops/multi-server-k8s-pgfix:$SHA
+docker push cygnetops/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server-k8s:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
