1017301f28c6baa87e784ee738e98a98096bfefe
Daniel Vela <divela@minsait.com>
2022-03-07 16:19:34 -0600

    Correccion de archivos travis y deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t divela/multi-client-k8s:latest -t divela/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t divela/multi-server-k8s-pgfix:latest -t divela/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t divela/multi-worker-k8s:latest -t divela/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t stephengrider/multi-client-k8s:latest -t stephengrider/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server-k8s:latest -t stephengrider/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker-k8s:latest -t stephengrider/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push divela/multi-client-k8s:latest
-docker push divela/multi-server-k8s-pgfix:latest
-docker push divela/multi-worker-k8s:latest
+docker push stephengrider/multi-client-k8s:latest
+docker push stephengrider/multi-server-k8s:latest
+docker push stephengrider/multi-worker-k8s:latest
 
-docker push divela/multi-client-k8s:$SHA
-docker push divela/multi-server-k8s-pgfix:$SHA
-docker push divela/multi-worker-k8s:$SHA
+docker push stephengrider/multi-client-k8s:$SHA
+docker push stephengrider/multi-server-k8s:$SHA
+docker push stephengrider/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=divela/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=divela/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=divela/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker-k8s:$SHA
